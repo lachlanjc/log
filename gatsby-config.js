cbc83a75d1c39e2ad0ddfbae34ee11a7f1f02b3e
Lachlan Campbell <lachlan.campbell@icloud.com>
2018-01-30 02:52:33 -0500

    Simplify dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,42 +3,31 @@ module.exports = {
     title: 'Daily Log',
     author: 'Lachlan Campbell',
     description: 'A daily log of my work.',
-    siteUrl: 'https://log.lachlanjc.me',
+    siteUrl: 'https://log.lachlanjc.me'
   },
   pathPrefix: '/',
   plugins: [
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/days`,
-        name: 'days',
-      },
+        name: 'days'
+      }
     },
     {
-      resolve: `gatsby-transformer-remark`,
+      resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
           {
-            resolve: `gatsby-remark-images`,
+            resolve: 'gatsby-remark-images',
             options: {
-              maxWidth: 512,
-            },
+              maxWidth: 512
+            }
           },
-          {
-            resolve: `gatsby-remark-responsive-iframe`,
-            options: {
-              wrapperStyle: `margin-bottom: 1.0725rem`,
-            },
-          },
-          'gatsby-remark-prismjs',
-          'gatsby-remark-copy-linked-files',
-          'gatsby-remark-smartypants',
-        ],
-      },
+          'gatsby-remark-smartypants'
+        ]
+      }
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    `gatsby-plugin-feed`,
-    `gatsby-plugin-react-helmet`,
-  ],
+    'gatsby-plugin-react-helmet'
+  ]
 }
